bca63814b95805bb6951334592b2a4cdcf3f6157
Alfred Kondoro <alfredkondoro97@gmail.com>
2020-08-01 13:04:44 +0300

    TabLayout and Fragment

---
+++ app/src/main/java/com/zulfycoding/devportfolioapp/views/AboutFragment.java
@@ -0,0 +1,36 @@
+package com.zulfycoding.devportfolioapp.views;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.zulfycoding.devportfolioapp.R;
+
+import androidx.fragment.app.Fragment;
+
+public class AboutFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public AboutFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View aboutView = inflater.inflate(R.layout.fragment_about, container, false);
+        // Inflate the layout for this fragment
+        return aboutView;
+    }
+}
